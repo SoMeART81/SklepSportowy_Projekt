eaf7cac2d7986ed55bbfeb77f0d3b6c05bef1f56
Artur Samek <Artur.Samek@microsoft.wsei.edu.pl>
2022-12-29 17:52:10 +0100

    Dodaj pliki projektów.

---
+++ SklepSportowy/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SklepSportowy.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
